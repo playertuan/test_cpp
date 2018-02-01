c27040f63ab6b5c202d3ec1b4233f62d21fde24a
tuan <tnguyen@ltsnet.net>
2018-02-01 09:47:43 -0500

    added design choice

--- triangle.cpp
+++ triangle.cpp
@@ -5,9 +5,11 @@
 #include <string>
 using namespace std;
 
-// helper function to return the triangle type
-int getTriangleType(int,int,int);
+// create enums for the three types of triangles
+enum Type { equilateral, isosceles, scalene };
 
+// helper function to return the triangle Type
+Type getTriangleType(int,int,int);
 
 int main()
 {
@@ -18,29 +20,44 @@ int main()
 
 	   cout << "Please enter 3 integers for the sides of the triangle: ";
 	   cin >> side1 >> side2 >> side3;
-       getTriangleType(side1,side2,side3);
+
 	   //cout << "You entered " << side1 << " " << side2 << " " << side3 << endl;
-       cin.clear();
+
+       Type type = getTriangleType(side1,side2,side3);
+
+       switch(type)
+       {
+          case equilateral:
+             cout << "equilateral" << endl;
+             break;
+          case isosceles:
+             cout << "isosceles" << endl;
+             break;
+          case scalene:
+             cout << "scalene" << endl;
+             break;
+          default:
+             cout << "unknown" << endl; 
+       }
 }
 
-int getTriangleType(int side1, int side2, int side3)
+// args: side1, side2, side3: the sides of the triangle
+// return: int: the type of traiangle
+Type getTriangleType(int side1, int side2, int side3)
 {
-   cout << "You entered " << side1 << " " << side2 << " " << side3 << endl;
    if ((side1 == side2) && (side2 == side3))
-   {
-      cout << "equilateral" << endl;
-      return 0;
-   }
+      return equilateral;
 
    if ((side1 == side2) || (side2 == side3) || (side1 == side3))
-   {
-      cout << "isosceles" << endl;
-      return 0;
-   }
+      return isosceles;
 
    if ((side1 != side2) && (side2 != side3) && (side1 != side3))
-   {
-      cout << "scalene" << endl;
-      return 0;
-   }
+      return scalene;
 }
+
+// At first I had getTriangleType just print out the type and return.
+// But then I switch it up to return enums so I can unit test it better
+// It is hard to unit test console output. So I made the function just
+// return the enume Type. The design choice to let the function do
+// one thing and do it every well, which is to classify the triangle type
+// and not worry about printing out the type into the console. 
